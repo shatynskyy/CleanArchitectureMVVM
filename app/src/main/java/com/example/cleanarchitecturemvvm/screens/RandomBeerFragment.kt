a7b7b5a513124290fc9f3c66526c7e14b612d1d4
Andriy Shatynskyy <andriy.shatynskyy@gmail.com>
2024-01-19 11:48:34 -0500

    Implemented Flow for RandomBeerViewModel

--- app/src/main/java/com/example/cleanarchitecturemvvm/screens/RandomBeerFragment.kt
+++ app/src/main/java/com/example/cleanarchitecturemvvm/screens/RandomBeerFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.bumptech.glide.Glide
 import com.example.cleanarchitecturemvvm.R
 import com.example.cleanarchitecturemvvm.viewmodels.RandomBeerViewModel
@@ -15,6 +18,7 @@ import com.example.cleanarchitecturemvvm.utility.ResponseWrapper
 import com.example.cleanarchitecturemvvm.utility.gone
 import com.example.cleanarchitecturemvvm.utility.visible
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class RandomBeerFragment : Fragment() {
@@ -39,36 +43,40 @@ class RandomBeerFragment : Fragment() {
     }
 
     private fun observeData() {
-        viewModel.randomBeerLiveData.observe(viewLifecycleOwner){ result ->
-            when(result){
-                is ResponseWrapper.Loading ->{
-                    binding.apply {
-                        binding.mainLayout.gone()
-                        binding.progressBar.visible()
-                        binding.errorLayout.gone()
-                    }
-                }
-                is ResponseWrapper.Success->{
-                    binding.apply {
-                        mainLayout.visible()
-                        progressBar.gone()
-                        errorLayout.gone()
-                        Glide.with(requireContext())
-                            .load(result.data.image)
-                            .placeholder(R.drawable.default_beer)
-                            .error(R.drawable.default_beer)
-                            .into(image)
-                        title.text = result.data.name
-                        abv.text = getString(R.string.abv_percent, result.data.abv)
-                        firstBrewed.text = result.data.firstBrewed
-                    }
-                }
-                is ResponseWrapper.Error->{
-                    binding.apply {
-                        mainLayout.gone()
-                        progressBar.gone()
-                        errorLayout.visible()
-                        errorTitle.text = ExceptionsWrapper.checkResponseError(exception = result.exception, context = requireContext())
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.randomBeerStateFlow.collect { result ->
+                    when(result){
+                        is ResponseWrapper.Loading ->{
+                            binding.apply {
+                                binding.mainLayout.gone()
+                                binding.progressBar.visible()
+                                binding.errorLayout.gone()
+                            }
+                        }
+                        is ResponseWrapper.Success->{
+                            binding.apply {
+                                mainLayout.visible()
+                                progressBar.gone()
+                                errorLayout.gone()
+                                Glide.with(requireContext())
+                                    .load(result.data.image)
+                                    .placeholder(R.drawable.default_beer)
+                                    .error(R.drawable.default_beer)
+                                    .into(image)
+                                title.text = result.data.name
+                                abv.text = getString(R.string.abv_percent, result.data.abv)
+                                firstBrewed.text = result.data.firstBrewed
+                            }
+                        }
+                        is ResponseWrapper.Error->{
+                            binding.apply {
+                                mainLayout.gone()
+                                progressBar.gone()
+                                errorLayout.visible()
+                                errorTitle.text = ExceptionsWrapper.checkResponseError(exception = result.exception, context = requireContext())
+                            }
+                        }
                     }
                 }
             }
--- app/src/main/java/com/example/cleanarchitecturemvvm/viewmodels/RandomBeerViewModel.kt
+++ app/src/main/java/com/example/cleanarchitecturemvvm/viewmodels/RandomBeerViewModel.kt
@@ -1,32 +1,32 @@
 package com.example.cleanarchitecturemvvm.viewmodels
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.cleanarchitecturemvvm.models.BeerModelPresentation
 import com.example.cleanarchitecturemvvm.models.toBeerPresentation
 import com.example.cleanarchitecturemvvm.utility.ResponseWrapper
-import com.example.data.exceptions.GeneralException
 import com.example.domain.models.BeerDomainModel
 import com.example.domain.usecases.GetRandomBeerUseCase
 import com.example.domain.usecases.InsertBeerUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class RandomBeerViewModel @Inject constructor(private val getRandomBeerUseCase: GetRandomBeerUseCase, private val insertBeerUseCase: InsertBeerUseCase): ViewModel() {
 
-    private val _randomBeerLiveData = MutableLiveData<ResponseWrapper<BeerModelPresentation>>()
-    var randomBeerLiveData : LiveData<ResponseWrapper<BeerModelPresentation>> = _randomBeerLiveData
+    private val _randomBeerStateFlow = MutableStateFlow<ResponseWrapper<BeerModelPresentation>>(ResponseWrapper.Loading())
+    val randomBeerStateFlow: StateFlow<ResponseWrapper<BeerModelPresentation>> = _randomBeerStateFlow.asStateFlow()
 
     private var beer: BeerDomainModel? = null
 
     private val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
-        _randomBeerLiveData.postValue(ResponseWrapper.Error(throwable))
+        _randomBeerStateFlow.value = ResponseWrapper.Error(throwable)
         throwable.printStackTrace()
     }
 
@@ -36,10 +36,10 @@ class RandomBeerViewModel @Inject constructor(private val getRandomBeerUseCase: 
 
     fun getRandomBeer(){
         viewModelScope.launch(Dispatchers.IO + coroutineExceptionHandler) {
-            _randomBeerLiveData.postValue(ResponseWrapper.Loading())
+            _randomBeerStateFlow.value = ResponseWrapper.Loading()
             val randomBeer = getRandomBeerUseCase.invoke()
             beer = randomBeer
-            _randomBeerLiveData.postValue(ResponseWrapper.Success(randomBeer.toBeerPresentation()))
+            _randomBeerStateFlow.value = ResponseWrapper.Success(randomBeer.toBeerPresentation())
         }
     }
 
